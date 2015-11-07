2a92ee7a162778c1a47dcdd38be64ed77615700d
Marco Täschner <spezialmann@me.com>
2015-11-07 10:42:11 +0100

    Add ObjectID_Serializer (for correct ObjectId format during REST communication) Add RawData object (raw WDE1-2 data logging)

---
+++ src/main/java/com/taeschma/domain/RawData.java
@@ -0,0 +1,42 @@
+package com.taeschma.domain;
+
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.taeschma.util.ObjectID_Serializer;
+
+@Document(collection = "StationRawDataCollection")
+public class RawData {
+	@Id
+    private ObjectId id;
+	
+	private Date timeOfRecording;
+    private String rawData;
+    
+    @JsonSerialize(using=ObjectID_Serializer.class) 
+	public ObjectId getId() {
+		return id;
+	}
+    @JsonSerialize(using=ObjectID_Serializer.class) 
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public Date getTimeOfRecording() {
+		return timeOfRecording;
+	}
+	public void setTimeOfRecording(Date timeOfRecording) {
+		this.timeOfRecording = timeOfRecording;
+	}
+	public String getRawData() {
+		return rawData;
+	}
+	public void setRawData(String rawData) {
+		this.rawData = rawData;
+	}
+    
+    
+}
