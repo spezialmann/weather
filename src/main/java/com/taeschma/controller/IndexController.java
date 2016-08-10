0f84b50e219fb85a339301339d82b1ae9afc333a
Marco Täschner <clubfahrer@me.com>
2016-08-11 00:03:37 +0200

    update rain chart

--- src/main/java/com/taeschma/controller/IndexController.java
+++ src/main/java/com/taeschma/controller/IndexController.java
@@ -22,21 +22,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.taeschma.util.WeatherMapper;
 
 /**
- *
  * @author marco
  */
 @Controller
 public class IndexController {
-    
+
     private final Logger log = LoggerFactory.getLogger(IndexController.class);
-        
+
     @Value("${weather.station.default}")
-	private String stationId;
+    private String stationId;
     @Value("${weather.station.show.current.index}")
-	private Integer measuringSpot;
-	@Value("${weather.stations.forecast.location.location.id}")
-	private String locationId;
-        
+    private Integer measuringSpot;
+    @Value("${weather.stations.forecast.location.location.id}")
+    private String locationId;
+
     @Autowired
     private RawDataService rawDataService;
     @Autowired
@@ -49,10 +48,10 @@ public class IndexController {
     private HourDataRepository hourDataRepository;
     @Autowired
     private RainService rainService;
-    
+
     /**
      * Index page with weather station info
-     * 
+     *
      * @param station
      * @param spot
      * @param model
@@ -60,42 +59,44 @@ public class IndexController {
      */
     @RequestMapping("/")
     public String index(
-    		@RequestParam(value = "station", defaultValue="", required = false) String station,
-    		@RequestParam(value = "spot", defaultValue="", required = false) Integer spot,
-    		Model model) {
+            @RequestParam(value = "station", defaultValue = "", required = false) String station,
+            @RequestParam(value = "spot", defaultValue = "", required = false) Integer spot,
+            Model model) {
         log.debug("index Action");
         List<StationRawData> findAllForToday = rawDataService.findAllForToday(station);
         List<CurrentStationWeather> currentStationWeatherList = WeatherMapper.getCurrentStationWeather(findAllForToday);
         log.debug("Anzahl: " + currentStationWeatherList.size());
-        
-        if(currentStationWeatherList!=null && currentStationWeatherList.size()>0) {
-        	model.addAttribute("lastUpdate", currentStationWeatherList.get(0).getTimestamp());
+
+        if (currentStationWeatherList != null && currentStationWeatherList.size() > 0) {
+            model.addAttribute("lastUpdate", currentStationWeatherList.get(0).getTimestamp());
         }
-        
-        
+
+
         model.addAttribute("currentStationWeatherList", currentStationWeatherList);
         model.addAttribute("currentStationWeather", weatherService.getCurrentWeatherForStationId(locationId));
-        
+
         return "index";
     }
-      
-    
+
+
     /**
      * Ajax request for current weather data
-     * 
+     *
      * @param location
      * @return
      */
     @RequestMapping("showcurrent")
-    public @ResponseBody CurrentWeather showCurrentWeather(@RequestParam(value = "location", defaultValue="", required = false) String location) {
-    	if(location==null || location.isEmpty()) {
-    		Location loc = locationService.findAll().get(0);
-    		location = loc.getLocationId();
-    	}
-    	log.debug("Location: " + location);
-    	Location loc = locationService.find(location);
-    	log.debug("Show weather for: " + loc.getLocationName());
-    	return weatherService.getCurrentWeatherForLocation(loc);
+    public
+    @ResponseBody
+    CurrentWeather showCurrentWeather(@RequestParam(value = "location", defaultValue = "", required = false) String location) {
+        if (location == null || location.isEmpty()) {
+            Location loc = locationService.findAll().get(0);
+            location = loc.getLocationId();
+        }
+        log.debug("Location: " + location);
+        Location loc = locationService.find(location);
+        log.debug("Show weather for: " + loc.getLocationName());
+        return weatherService.getCurrentWeatherForLocation(loc);
     }
 
 
@@ -106,36 +107,11 @@ public class IndexController {
     }
 
     @RequestMapping(value = "/rain", method = RequestMethod.GET)
-    public String rainAnalytics(Model model) {
-
-
+    public String rainChart(Model model) {
         List<DiagramData> rain = rainService.getRain();
-
-        StringBuilder keys = new StringBuilder("[");
-        StringBuilder vals = new StringBuilder("[");
-
-        int i = 0;
-        for (DiagramData dd : rain) {
-            if(i>0) {
-                keys.append(", ");
-                vals.append(", ");
-            }
-
-            keys.append("\"" + dd.getHour() + "\"");
-            vals.append("\"" + dd.getDataValue() + "\"");
-
-            i++;
-        }
-        keys.append("]");
-        vals.append("]");
-
-        log.info("Anzahl: " + rain.size());
         log.info(rain.toString());
-        log.info(keys.toString());
-        log.info(vals.toString());
 
-        model.addAttribute("keys", keys);
-        model.addAttribute("vals", vals);
+        model.addAttribute("rain", rain);
 
         return "rain";
     }
