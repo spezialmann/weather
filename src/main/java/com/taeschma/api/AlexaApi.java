611456841774e801c998f37fc0625ee01c7e5e9d
Marco Täschner <clubfahrer@me.com>
2017-11-17 22:07:40 +0100

    change Alexa Api

--- src/main/java/com/taeschma/api/AlexaApi.java
+++ src/main/java/com/taeschma/api/AlexaApi.java
@@ -2,6 +2,9 @@ package com.taeschma.api;
 
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.SimpleCard;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,32 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class AlexaApi {
-    
+
     @RequestMapping("/alexa/api")
     public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         log.info("AlexaApi@hello");
-        
-        
-        
-        
-        
-        
+
         return "{\"version\":\"1.0\",\"response\":{\"outputSpeech\":{\"type\":\"PlainText\",\"text\":\"Heute hat es bisher 2,1 Liter geregnet.\",\"ssml\":null},\"shouldEndSession\":true}}";
     }
-    
+
     @RequestMapping("/alexa/amazon/api")
-    public String amazonApi(IntentRequest intentRequest) {
+    public SpeechletResponse amazonApi(IntentRequest intentRequest) {
         log.info("AlexaApi@amazonApi");
-        
-        
-        Intent intent = intentRequest.getIntent();
-        log.info(intent.toString());
-        
-        
-        
-        return "{\"version\":\"1.0\",\"response\":{\"outputSpeech\":{\"type\":\"PlainText\",\"text\":\"Heute hat es bisher 2,1 Liter geregnet.\",\"ssml\":null},\"shouldEndSession\":true}}";
+
+        String speechText = "Hello world";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return SpeechletResponse.newTellResponse(speech, card);
+
     }
-    
-    
-    
+
 }
