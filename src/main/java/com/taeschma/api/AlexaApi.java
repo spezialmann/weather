96de385e0f7d6d336990b9e63ea7c77057095d77
Marco Täschner <clubfahrer@me.com>
2017-11-18 00:15:00 +0100

    V1 Alexa Api

--- src/main/java/com/taeschma/api/AlexaApi.java
+++ src/main/java/com/taeschma/api/AlexaApi.java
@@ -5,6 +5,17 @@ import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.SimpleCard;
+import com.taeschma.domain.CurrentStationWeather;
+import com.taeschma.domain.StationRawData;
+import com.taeschma.service.RawDataService;
+import com.taeschma.util.WeatherMapper;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -24,6 +35,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlexaApi {
 
+    private RawDataService rawDataService;
+
+    public AlexaApi(RawDataService rawDataService) {
+        this.rawDataService = rawDataService;
+    }
+    
     @RequestMapping("/alexa/api")
     public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         log.info("AlexaApi@hello");
@@ -35,8 +52,7 @@ public class AlexaApi {
     public AlexaResponse amazonApi() {
         log.info("AlexaApi@amazonApi");
 
-        
-        String speechText = "Hello world";
+        String speechText = getStationWeather();
 
         // Create the Simple card content.
         SimpleCard card = new SimpleCard();
@@ -48,13 +64,64 @@ public class AlexaApi {
         speech.setText(speechText);
 
         SpeechletResponse newTellResponse = SpeechletResponse.newTellResponse(speech);
-        
+
         log.info(newTellResponse.toString());
-        
+
         AlexaResponse alexaResponse = new AlexaResponse("1.0", newTellResponse);
-        
+
         return alexaResponse;
 
+    }
+    
+    private String getStationWeather() {
+        String alexaAnsage = "Das sind die aktuellen Daten der Wetterstation vom ";
+        
+        List<StationRawData> findAllForToday = rawDataService.findAllForToday("");
+        List<CurrentStationWeather> currentStationWeatherList = WeatherMapper.getCurrentStationWeather(findAllForToday);
+        
+        
+
+        if (null != currentStationWeatherList && currentStationWeatherList.size() > 0) {
+            log.debug("Anzahl: " + currentStationWeatherList.size());
+            
+            CurrentStationWeather aussen = currentStationWeatherList.get(0);
+            Date lastUpdate = aussen.getTimestamp();
+
+            log.info(lastUpdate.toString());
+
+            ZoneId defaultZoneId = ZoneId.of("CET");
+            //1. Convert Date -> Instant
+            Instant instant = lastUpdate.toInstant();
+            //System.out.println("instant : " + instant); //Zone : UTC+0
+            //3. Instant + system default time zone + toLocalDateTime() = LocalDateTime
+            LocalDateTime localDateTime = instant.atZone(defaultZoneId).toLocalDateTime();
+            log.debug("localDateTime : " + localDateTime);
+            DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.GERMAN);
+            DateTimeFormatter formatterHour = DateTimeFormatter.ofPattern("HH", Locale.GERMAN);
+            DateTimeFormatter formatterMinute = DateTimeFormatter.ofPattern("mm", Locale.GERMAN);
+
+            String formatDate = localDateTime.format(formatterDate);
+            String formatHour = localDateTime.format(formatterHour);
+            String formatMinute = localDateTime.format(formatterMinute);
+            
+            alexaAnsage += formatDate + " um " + formatHour + " Uhr " + formatMinute;
+            
+            alexaAnsage += " Aktuelle Außentemperatur " + aussen.getTemperature().toString().replace('.', ',') + " Grad ";
+            alexaAnsage += " Tageshöchsttemperatur " + aussen.getMaxTemperature().toString().replace('.', ',') + " Grad ";
+            alexaAnsage += " Tagesminimum " + aussen.getMinTemperature().toString().replace('.', ',') + " Grad ";
+            alexaAnsage += " Regenmenge heute " + aussen.getPrecipMMSum().toString().replace('.', ',') + " Liter pro Quadratmeter ";
+            
+            if(currentStationWeatherList.size()>1) {
+                CurrentStationWeather innen = currentStationWeatherList.get(1);
+                alexaAnsage += " Aktuelle Innentemperatur " + innen.getTemperature().toString().replace('.', ',') + " Grad ";
+            }
+            
+        }
+        else {
+            alexaAnsage = "Aktuelle keine Wetterdaten verfügbar";
+        }
+        
+        return alexaAnsage;
     }
 
 }
@@ -63,6 +130,7 @@ public class AlexaApi {
 @NoArgsConstructor
 @RequiredArgsConstructor
 class AlexaResponse {
+
     @NonNull
     private String version;
     @NonNull
